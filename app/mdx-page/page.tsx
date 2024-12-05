72936efd8d35f4b4acf1017316e1460eb1ca1519
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2024-12-05 20:00:02 +0700

    Fix issue - next-mdx-remote

--- app/mdx-page/page.tsx
+++ app/mdx-page/page.tsx
@@ -1,6 +1,6 @@
-import MdxLayout from './layout';
+import Welcome from '@/blogs/ai-is-taking-your-job.mdx';
 
-import Welcome from '@/markdown/ai-is-taking-your-job.mdx';
+import MdxLayout from './layout';
 
 export default function Page() {
   return (

31b20dd07226f2b192c776f29177146c06347bf1
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2024-12-06 21:07:53 +0700

    Create first blog test

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -1,12 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import matter from 'gray-matter';
 
-export default function Home() {
+import MdxLayout from '../layout';
+
+export async function generateStaticParams() {
+  const files = fs.readdirSync(path.join('blogs'));
+
+  const paths = files.map((filename) => ({
+    slug: filename.replace('.mdx', ''),
+  }));
+
+  return paths;
+}
+function getPost({ slug }: { slug: string }) {
+  const markdownFile = fs.readFileSync(
+    path.join('blogs', slug + '.mdx'),
+    'utf-8'
+  );
+
+  const { data: frontMatter, content } = matter(markdownFile);
+
+  return {
+    frontMatter,
+    slug,
+    content,
+  };
+}
+export default function Post({ params }: any) {
+  const props = getPost(params);
+
   return (
-    <MDXRemote
-      source={`# Hello World
+    <MdxLayout>
+      <h1>{props.frontMatter.title}</h1>
+      <h5>{props.frontMatter.description}</h5>
+      <h5>{props.frontMatter.date}</h5>
 
-      This is from Server Components!
-      `}
-    />
+      <MDXRemote source={props.content} />
+    </MdxLayout>
   );
 }

755cd0ac42829809782b3f21b885b95588f2b086
Vu Thi Bich Thu <vuthibichthu.dev@gmail.com>
2024-12-07 18:35:45 +0700

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+
 export default async function About() {
-  return <div>About</div>;
+  return (
+    <div className="prose">
+      <h2>About myself</h2>
+      <Image src="/images/photo.jpg" alt="my-photo" width={400} height={400} />
+      <p>
+        Hi everyone! I&apos;m Thu, a software engineer with a passion for
+        coding.
+        <br />
+        I&apos;m excited to share my experiences, learn from others, and connect
+        with like-minded individuals.
+      </p>
+    </div>
+  );
 }
